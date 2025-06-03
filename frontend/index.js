d94b7fc965205de9da9e15ec09b6bafdaab63d17
Mark-74 <balduccimarco2007@gmail.com>
2025-06-03 11:17:33 +0200

    added basic frontend pages

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// express settings
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+// static files
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/bootstrap-icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons')));
+app.set('views', path.join(__dirname, 'templates'));
+
+// routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/results', (req, res) => {
+    const domain = req.query.domain;
+    
+    res.render('results', { domain: domain });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
