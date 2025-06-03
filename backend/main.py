103d77fc184443905bd73d6b7e1785924b6014fc
AlBovo <alandavide.bovo.stud@ispascalcomandini.it>
2025-06-03 14:31:57 +0200

    updated apis and added async handler

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,42 @@
-#!/usr/env/python3
-from flask import Flask, request, jsonify
-from scraper import scrape_website, scrape_dorks
+import json
+from uuid import uuid4
+from flask import Flask, request, jsonify, send_file
+from scraper import handle_request
 
 app = Flask(__name__)
 
-@app.route('/api/search', methods=['GET'])
-def search():
-    domain = request.args.get('domain')
+@app.route('/api/search', methods=['POST'])
+async def search():
+    domain = request.json.get('domain')
+
     if not domain:
         return jsonify({"error": "Domain parameter is required"}), 400
-    
-    results = []
-    results.extend(scrape_website(domain))
-    results.extend(scrape_dorks(domain))
 
-    return jsonify(results)
+    id = str(uuid4())
+    handle_request(domain, id)
+
+    return jsonify({'id': id})
+
+@app.route('/api/fetch', methods=['GET'])
+def fetch():
+    id = request.json.get('id')
+    if not id:
+        return jsonify({"error": "ID parameter is required"}), 400
+    d = json.load(open(f'./data/{id}.json', 'r'))
+    
+    idx = d.get('index')
+    status = d.get('status', 'pending')
+    emails = d.get('emails', [])
+
+    if not idx:
+        return jsonify({"error": "No data found for the given ID"}), 500
+    
+    if idx >= len(emails):
+        return jsonify([]), 200
+
+    d['index'] = len(emails)
+    json.dump(d, open(f'./data/{id}.json', 'w'))
+    return jsonify(emails[idx:]), 418 if status != 'pending' else 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/scraper.py
+++ backend/scraper.py
@@ -1,14 +1,38 @@
-import requests, re, tqdm
+import requests
+import tqdm
+import json
+import re
 from bs4 import BeautifulSoup
 from googlesearch import search
 from urllib.parse import urlencode
 
 DEPTH_LIMIT = 10
 
-def scrape_website(url: str, depth=0) -> list[str]:
-    if depth >= DEPTH_LIMIT:
-        return []
+async def handle_request(url: str, id: str):
+    emails = scrape_website(url, set())
+    emails += scrape_dorks(url)
+
+    with open(f'./data/{id}.json', 'r+') as f:
+        d = json.load(f)
+        d['status'] = 'completed'
+        f.seek(0)
+        json.dump(d, f)
+        f.truncate()
+
     
+async def save_data(id: str, emails: list[str]):
+    with open(f'./data/{id}.json', 'r+') as f:
+        d = json.load(f)
+        d['emails'] += emails
+        f.seek(0)
+        json.dump(d, f)
+        f.truncate()
+
+async def scrape_website(url: str, visited: set, depth=0) -> list[str]:
+    if depth >= DEPTH_LIMIT or url in visited:
+        return []
+
+    visited.add(url)
     email = r'[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}'
     try:
         r = requests.get(url)
@@ -23,14 +47,14 @@ def scrape_website(url: str, depth=0) -> list[str]:
     # BFS search for more emails
     soup = BeautifulSoup(r.text, 'html.parser')
     for a in tqdm(soup.find_all('a', href=True), desc=f"Scraping {url}"):
-        values += scrape_website(a['href'], depth + 1)
-    
+        values += scrape_website(a['href'], visited, depth + 1)
+
     values = list(set(values))
     values = [v.lower().strip() for v in values] 
 
     return values
 
-def scrape_dorks(domain: str) -> list[str]:
+async def scrape_dorks(domain: str) -> list[str]:
     query = f"site:{domain} intext:@{domain}"
     url = f"https://google.co.in/search?{urlencode({'q': query})}"
 
@@ -46,6 +70,6 @@ def scrape_dorks(domain: str) -> list[str]:
     for url in res:
         if domain not in url:
             continue
-        values += scrape_website(url)
+        values += scrape_website(url, set())
 
     return values
