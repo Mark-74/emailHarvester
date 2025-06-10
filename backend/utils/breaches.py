34b3f722afb70e303966bb75028155c6c4b1bdf8
Mark-74 <balduccimarco2007@gmail.com>
2025-06-10 09:25:01 +0200

    removed selenium and added api

--- backend/utils/breaches.py
+++ backend/utils/breaches.py
@@ -1,18 +1,5 @@
-import requests, time
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
+import requests
 
 def is_breached(domain: str) -> bool:
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-
-    with webdriver.Firefox(options=options) as driver:
-        driver.get(f'https://www.breachsense.com/check-your-exposure/?domain={domain}')
-        time.sleep(3)
-        page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-    return soup.find('h2').text == 'Data Breach Detected'
+    data = requests.get(f'https://api.breachsense.com/stats?s={domain}', headers={'Origin': 'https://breachsense.com'}).json()
+    return data.get('emp') > 0 or data.get('usr') > 0
